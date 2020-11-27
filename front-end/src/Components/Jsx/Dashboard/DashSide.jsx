9ed71d3dc0e06f722b04ae3a4c1a7d213ad79ab1
Aishh2k <aiswarya2000may@gmail.com>
2020-11-27 22:04:04 +0530

    sidebar added

--- front-end/src/Components/Jsx/Dashboard/DashSide.jsx
+++ front-end/src/Components/Jsx/Dashboard/DashSide.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link} from 'react-router-dom';
+import { SidebarData } from './SidebarData'
+import '../../Css/Sidebar.css'
 
 class DashSide extends Component {
     render() {
         return (
             <div id="sidebar">
+                 <ul className='sidebar-items'>
+                {SidebarData.map((item, index) => {
+                return (
+                    <li key={index} className={item.cName}>
+                    <FontAwesomeIcon className="sidebar-icon" icon={ item.icon } />
+                    <Link to={item.path}>
+                        <p>{item.title}</p>
+                    </Link>
+                    </li>
+              );
+            })}
+
+
+
+
+             </ul>
+
                 
             </div>
         )
