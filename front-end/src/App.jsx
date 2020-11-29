8be8e5187b4f66ce4c37646f65ffa9f1eb663734
G-M-C <hurakannx@gmail.com>
2020-11-29 17:59:09 +0530

    Removed unwanted routing from landing page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,9 +3,6 @@ import './App.css'
 import Login from './Components/Jsx/Login'
 import Register from './Components/Jsx/Register'
 import Dashboard from './Components/Jsx/Dashboard/Dashboard'
-import Booking from './Components/Jsx/Dashboard/Pages/Booking'
-import Payment from './Components/Jsx/Dashboard/Pages/Payment'
-//Payment imported here only for testing , /payment shouldn't be accesible from main page in final version
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
@@ -18,7 +15,7 @@ export class App extends Component {
                 <Route exact path="/Login" component={Login} />
                 <Route path="/Dashboard" component={ Dashboard } />  
                 <Route exact path="/Register" component={Register} /> 
-                <Route exact path="/Payment" component={Payment} />
+                
                 
                       
               
